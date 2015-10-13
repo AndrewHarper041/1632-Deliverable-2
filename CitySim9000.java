9e02c1b781f8df1825ec00b43a588b868cd223f8
Andrew Harper <graphitezeppelin@gmail.com>
2015-10-13 14:27:42 -0400

    main refactored

--- CitySim9000.java
+++ CitySim9000.java
@@ -12,19 +12,29 @@ public class CitySim9000
             
         else
         {
-            lcg = new LCG(Integer.parseInt(args[0])); //create lcg with seed as input
-            drivers = new Drivers(lcg); //init drivers object
-            drivers.genDrivers(); //generate random location for list of drivers
- 
-            //Iterate over the 5 drivers, each driving randomly until location returns as "Outside City"
-            for(int i=0; i<5; i++)
+            mainInit(Integer.parseInt(args[0])); //setup necessary objects
+            driveSim();
+        }
+    }
+    
+    //Creates LCG, Drivers, and randomly makes the 5 drivers
+    public static void mainInit(int seed)
+    {
+    	lcg = new LCG(seed); //create lcg with seed as input
+        drivers = new Drivers(lcg); //init drivers object
+        drivers.genDrivers(); //generate random location for list of drivers
+    }
+    
+    //Iterates over the 5 drivers, each driving randomly until location returns as "Outside City" and outputs to console what is happening
+    public static void driveSim()
+    {
+    	for(int i=0; i<5; i++)
+        {
+            do //do-while used so drivers starting from outside city get to drive at least once before termination
             {
-                do //do-while used so drivers starting from outside city get to drive at least once before termination
-                {
-                    drivers.drive(i); //drive function will print out to command line what is occuring
-                } while(!drivers.getDriverLoc(i).equals("Outside City"));
-                System.out.printf("Driver " + i + " has left the city!\n-----\n");
-            }
+                drivers.drive(i); //drive function will print out to command line what is occuring
+            } while(!drivers.getDriverLoc(i).equals("Outside City"));
+            System.out.printf("Driver " + i + " has left the city!\n-----\n");
         }
     }
 }
